3e68625492ce6590cb0bfc4bdd839c42a42f4d91
brunocriacoes <br.rafael@outlook.com>
2023-05-19 15:18:27 -0300

    start fix

--- controle/Test.php
+++ controle/Test.php
@@ -6,6 +6,23 @@ class Test extends \core\Controle
 {
     static function debug() {
 
+        $db = new \core\Banco();
+        $inst = new \model\Institution($db);
+
+        $inst->register(
+            "BRC 1",
+            "#C00",
+            "brc.png",
+            "inst_642d98697d8e6",
+            "Bruno Criações",
+            "5582999776698",
+            "br.rafael@outlook.com"
+        );
+
+        self::printSuccess(
+            "testa cadastro institution ",
+            []
+        );
        
     }
 }
